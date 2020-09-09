34a4d8914444c9940529cf79c5e676bf729317dc
Justus Witmer <justus.witmer@gmail.com>
2020-09-09 11:41:49 -0500

    wired up database

--- server/server.js
+++ server/server.js
@@ -11,6 +11,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // globals
 const port = 3000;
 
+// db setup
+const Pool = pg.Pool;
+// configure connectio to db
+const pool = new Pool({
+    database: "music_library", // db name (not the table name)
+    host: "localhost", // default  when running locally, will change when deployed
+    port: 5432, // default port for local, also will change when deployed
+    max: 12, // max # of connections
+    idleTimeoutMillis: 20000 // connection timeout in milliseconds
+}); // end pool setup
+
 // spin
 app.listen(port, () => {
     console.log('server up:', port);
@@ -18,7 +29,15 @@ app.listen(port, () => {
 
 app.get('/songs', (req, res) => {
     console.log('in/songs GET');
-    res.send('ribbet')
+    // test query: top 40 songs by rank
+    // SELECT * FROM "songs" ORDER BY "rank" DESC LIMIT 40;
+    const queryString = 'SELECT * FROM "songs" ORDER BY "rank" DESC LIMIT 40;';
+    pool.query(queryString).then((results) => {
+        res.send(results.rows); // runs if query was successful
+    }).catch((err) => {
+        console.log(err); // if there was an error running query
+        res.sendStatus(500);
+    }) // end query
 }) // end /songs GET
 
 app.post('/songs', (req, res) => {
